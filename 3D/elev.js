e65ddc89b98c7e4bcc19891bf2a99a624191549a
James FL Thompson <jfth225@uky.edu>
2025-04-27 21:06:55 -0400

    edit

--- 3D/elev.js
+++ 3D/elev.js
@@ -3,8 +3,10 @@
 // import * as d3 from "d3";
 // import * as turf from "@turf/turf";
 
+import { PMTiles } from "pmtiles";
+
 // Set up pmtiles protocol
-let protocol = new pmtiles.Protocol();
+let protocol = new PMTiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
 let map;
